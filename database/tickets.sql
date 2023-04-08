ade27f978154128163197a55e5545e1b3eb4557c
David Ferreira <davidferreira@Air-de-David-2.home>
2023-04-08 15:11:45 +0100

    new database

--- database/tickets.sql
+++ database/tickets.sql
@@ -1,74 +1,49 @@
- DROP TABLE IF EXISTS users;
- DROP TABLE IF EXISTS departments;
- DROP TABLE IF EXISTS tickets;
- DROP TABLE IF EXISTS hashtags;
- DROP TABLE IF EXISTS faqs;
-
-
-
 CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  username TEXT NOT NULL,
-  name Text NOT NULL,
-  email TEXT NOT NULL UNIQUE,
+  id AUTOINCREMENT PRIMARY KEY,
+  name TEXT NOT NULL,
+  username TEXT NOT NULL UNIQUE,
   password TEXT NOT NULL,
-
+  email TEXT NOT NULL UNIQUE,
+  role TEXT NOT NULL
 );
 
 CREATE TABLE departments (
-  id INTEGER PRIMARY KEY,
-  nameDepartment TEXT NOT NULL
-);
-
-CREATE TABLE tickets (
-  id INTEGER PRIMARY KEY,
-  user_id INTEGER NOT NULL,
-  department_id INTEGER NOT NULL,
-  theme TEXT NOT NULL,
-  description TEXT NOT NULL,
-  status TEXT NOT NULL DEFAULT 'Depend',
-  hastag_name TEXT NOT NULL, 
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (department_id) REFERENCES departments (id),
-  FOREIGN KEY (hastag_name) REFERENCES hastags (name)
+  id AUTOINCREMENT PRIMARY KEY,
+  name TEXT NOT NULL
 );
 
 CREATE TABLE hashtags (
-  id INTEGER PRIMARY KEY,
+  id AUTOINCREMENT PRIMARY KEY,
   name TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE faqs (
-  id INTEGER PRIMARY KEY,
-  question TEXT NOT NULL,
-  answer TEXT NOT NULL,
-  hashtag_name Text NOT NULL,
-  FOREIGN KEY (hashtag_name) REFERENCES hashtags (name)
+CREATE TABLE tickets (
+  id AUTOINCREMENT PRIMARY KEY,
+  subject TEXT NOT NULL,
+  description TEXT NOT NULL,
+  status TEXT NOT NULL,
+  priority TEXT NOT NULL,
+  department_id INTEGER NOT NULL,
+  user_id INTEGER NOT NULL,
+  agent_id INTEGER,
+  created_at TEXT NOT NULL,
+  updated_at TEXT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES departments (id),
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (agent_id) REFERENCES users (id)
 );
 
-Create Table admin(
-    id SERIAL PRIMARY KEY,
-    id_user INTEGER NOT NULL,
-    FOREIGN KEY (id_user) REFERENCES users(id)
+CREATE TABLE ticket_hashtags (
+  ticket_id INTEGER NOT NULL,
+  hashtag_id INTEGER NOT NULL,
+  PRIMARY KEY (ticket_id, hashtag_id),
+  FOREIGN KEY (ticket_id) REFERENCES tickets (id),
+  FOREIGN KEY (hashtag_id) REFERENCES hashtags (id)
+);
 
-)
-
-
-Insert INTO users (id,username,name,email,password,admin) VALUES(0,'david2002','David','davidferreira@gmail.com','DaVid');
-Insert INTO users (id,username,name,email,password,admin) VALUES(1,'John29233','John','John@gmail.com','JoHn');
-Insert INTO users (id,username,name,email,password,admin) VALUES(2,'Carol49359','Carol','Carolo@gmail.com','carolina');
-Insert INTO users (id,username,name,email,password,admin) VALUES(3,'Freitas3131','Freitas','Freitas@gmail.com','gatos');
-Insert INTO users (id,username,name,email,password,admin) VALUES(4,'Rute10301','Rute','Rute@gmail.com','porto','0');
-Insert INTO users (id,username,name,email,password,admin) VALUES(5,'Paulo10193','Paulo','Paulo@gmail.com','bola','0');
-Insert INTO users (id,username,name,email,password,admin) VALUES(6,'Monica23921','Monica','Monica@gmail.com','portugal');
-Insert INTO users (id,username,name,email,password,admin) VALUES(7,'Leonor298429','Leonor','Leonor@gmail.com','chocolate');
-Insert INTO users (id,username,name,email,password,admin) VALUES(8,'Vasco139138','Vasco','Vasco@gmail.com','lisboa');
-
-
-
-
-
+CREATE TABLE faqs (
+  id AUTOINCREMENT PRIMARY KEY,
+  question TEXT NOT NULL UNIQUE,
+  answer TEXT NOT NULL
+);
 
