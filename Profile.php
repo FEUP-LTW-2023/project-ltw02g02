921065da3393c96dd997265c8979b9f612338d4d
David Ferreira <davidferreira@Air-de-David-2.home>
2023-04-08 12:13:54 +0100

    login register done with css

---
+++ Profile.php
@@ -0,0 +1,6 @@
+<?php
+	$name = "John Doe";
+	$profile_image = "profile.jpg";
+	$bio = "Hi, I'm John Doe. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel sapien ultrices, faucibus magna in, ultrices nibh. Proin blandit molestie magna at faucibus. Praesent ut ipsum elit. Donec euismod vel sapien ut consequat. In ut lobortis nisi. Sed euismod lobortis quam, eget tempus tellus hendrerit et. Fusce in aliquet nibh. Suspendisse auctor mauris quis libero interdum, eu tincidunt justo bibendum. Donec id urna id nunc blandit auctor.";
+	$skills = array("HTML", "CSS", "JavaScript", "PHP");
+?>
