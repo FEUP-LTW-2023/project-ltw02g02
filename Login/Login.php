dff91507afa6e1e6e7ba2d175067c367325e340f
David Ferreira <davidferreira@MacBook-Air-de-David-2.local>
2023-04-13 14:50:29 +0100

    register,login,main,profile almost done

---
+++ Login/Login.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+require_once('db.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $stmt = $pdo->prepare("SELECT * FROM users WHERE username = :username");
+  $stmt->execute(['username' => $username]);
+  $user = $stmt->fetch();
+
+  if ($user && password_verify($password, $user['password'])) {
+    $_SESSION['user_id'] = $user['id'];
+    $_SESSION['username'] = $user['username'];
+    $_SESSION['role'] = $user['role'];
+    header('Location: index.php');
+  } else {
+    $error = "Invalid username or password";
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="login-page">
+    <div class="form">
+      <form class="login-form" method="post" action="login.php">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+        <p class="message">Not registered? <a href="register.php">Create an account</a></p>
+        <?php if (isset($error)) { ?>
+          <p class="error"><?php echo $error; ?></p>
+        <?php } ?>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
---
+++ Profile/Profile.php
@@ -0,0 +1,30 @@
+<?php
+// check if user is logged in
+session_start();
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Profile Page</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<h1>Profile</h1>
+		<div class="profile">
+			<img src="profile_pic.png" alt="Profile Picture">
+			<div class="info">
+				<h2>Name: John Doe</h2>
+				<h3>Email: johndoe@email.com</h3>
+				<p>Bio: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisi a velit sollicitudin dignissim. Donec vel neque vel orci accumsan scelerisque.</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
